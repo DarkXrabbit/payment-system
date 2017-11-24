47a36dd5c0f1891f9004034d66d9ef518aeee73d
在下就是俠兔醬也 <darkxrabbit@hotmail.com>
2017-11-24 21:01:46 +0800

    In instruction 🚀

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-print "abc";
-?>
+<!DOCTYPE html>
+<html lang="zh-Hant" id="payment">
+    <head>
+        <title>Building!</title>
+    </head>
+    <body>
+        <center>
+            <h1>
+                The system is in instruction!
+            </h1>
+        </center>
+    </body>
+</html>
